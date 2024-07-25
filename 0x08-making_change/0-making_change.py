4d8b01e84eeb32b82d3271a279198614d85103b6
zelalem3 <zgetnet24@example.com>
2024-07-25 11:32:54 -0700

    edited file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,20 @@
-
 #!/usr/bin/python3
-"""Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
+"""Change making module.
+"""
 
 
 def makeChange(coins, total):
-    """Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet a given amount total"""
     if total <= 0:
         return 0
 
-    current_total = 0
-    coin_used = 0
-    coins = sorted(coins, reverse=True)
-    for coin in coins:
-        r = (total-current_total)//coin
-        current_total += r*coin
-        coin_used += r
-        if current_total == total:
-            return coin_used
-    return -1
+    # Create an array to store the minimum number of coins required to reach each value
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
 
+    for coin in coins:
+        for i in range(coin, total + 1):
+            # Update the minimum number of coins required for each value
+            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+
+    # Return the minimum number of coins required to reach the total value
+    return min_coins[total] if min_coins[total] != float('inf') else -1
